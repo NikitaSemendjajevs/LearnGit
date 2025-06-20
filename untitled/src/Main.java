2d844bad97fa626c082416bbd01d7d4f719cbf8e
Nikita Semendjajevs <nikitasemendjajevs@yahoo.com>
2025-06-20 18:17:52 +0100

    add 'sum' method

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-	System.out.println("Hi, my name is Nikita Semendjajevs");	
+
+	System.out.println("Hi, my name is Nikita Semendjajevs");
+
+	sum(3, 7);
+
+	public static void sum(int a, int b) {
+		System.out.println(a + b);
+	}
     }
 }
